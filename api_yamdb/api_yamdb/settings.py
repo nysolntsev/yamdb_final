7f7725ac7aaf1316ea3b44ca8205ec192fe4233a
nysolntsev <95509550+nysolntsev@users.noreply.github.com>
2022-11-04 00:54:38 +0300

    0311 try

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -64,11 +64,11 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'NAME': os.getenv('DB_NAME', default='username'),
+        'USER': os.getenv('POSTGRES_USER', default='username'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='password'),
+        'HOST': os.getenv('DB_HOST', default='host'),
+        'PORT': os.getenv('DB_PORT', default='port')
     }
 }
 
